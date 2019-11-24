bf80e8a44d2d485c715a4bdaba07525e11ea32a5
Nagma Kapoor <nagmakapoor@gmail.com>
2019-11-24 16:50:21 -0500

    added code to save tweets to csv

--- Twitter_connection.py
+++ Twitter_connection.py
@@ -8,6 +8,7 @@ except ImportError:
 # Import the tweepy library
 import tweepy
 import urllib
+import csv
 
 ACCESS_TOKEN = '772178815414050816-KeN292eY2t0TDNC6h3EpK7VR1qIBKgK'
 ACCESS_SECRET = '1iohtKtnbAs3qHryyhgIHU2hLSaTofjx6bbq7jam698QZ'
@@ -26,6 +27,12 @@ abusive_words = 'fuck OR bitch OR cunt OR dumb OR ugly OR useless OR ignored OR 
 
 abusive_words.replace(" ", "%20")
 
+# Open/create a file to append data to
+csvFile = open('abusive_tweets.csv', 'a')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
+
 public_tweets = tweepy.Cursor(api.search, q=abusive_words, tweet_mode='extended', count=100)
 api.home_timeline()
 count = 0
@@ -33,8 +40,12 @@ for tweet in public_tweets.items(100):
     # print(tweet.text)
     if tweet.lang == "en":
         print(count)
-        print(tweet.full_text)
+        # Write a row to the CSV file. I use encode UTF-8
+        csvWriter.writerow([tweet.created_at, tweet.full_text.encode('utf-8')])
+        print(tweet.created_at, tweet.full_text)
         count = count + 1
+
+csvFile.close()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Twitter API development use pagination for Iterating through timelines, user lists, direct messages, etc.
