28f46f697b4319f1ffd1b8b1e2888472c828feda
Nagma Kapoor <nagmakapoor@gmail.com>
2019-11-24 17:57:41 -0500

    merged

--- Twitter_connection.py
+++ Twitter_connection.py
@@ -9,6 +9,7 @@ except ImportError:
 import tweepy
 import urllib
 import csv
+import preprocessor as p
 
 ACCESS_TOKEN = '772178815414050816-KeN292eY2t0TDNC6h3EpK7VR1qIBKgK'
 ACCESS_SECRET = '1iohtKtnbAs3qHryyhgIHU2hLSaTofjx6bbq7jam698QZ'
@@ -23,30 +24,31 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 # Create the api to connect to twitter with your creadentials
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
-abusive_words = 'fuck OR bitch OR cunt OR dumb OR ugly OR useless OR ignored OR rumors OR spread OR teased OR crying OR bullying OR abuse OR hit OR neglect OR hate OR racist OR die OR kill OR hurt'
+negative_words = 'fuck OR bitch OR cunt OR dumb OR ugly OR useless OR ignored OR rumors OR spread OR teased OR crying OR bullying OR abuse OR hit OR neglect OR hate OR racist OR die OR kill OR hurt'
 positive_words = 'happy OR great OR good OR proud OR joy OR friends OR family OR confident OR love OR peace OR fine OR important'
 
-abusive_words.replace(" ", "%20")
+negative_words.replace(" ", "%20")
 positive_words.replace(" ", "%20")
 
 # Open/create a file to append data to
-csvFile = open('abusive_tweets.csv', 'a')
+csvFile = open('tweets.csv', 'a')
 
 #Use csv writer
 csvWriter = csv.writer(csvFile)
 
-positive_tweets = tweepy.Cursor(api.search, q=positive_words, tweet_mode='extended', count=100)
-public_tweets = tweepy.Cursor(api.search, q=abusive_words, tweet_mode='extended', count=100)
+positive_tweets = tweepy.Cursor(api.search, q=positive_words, tweet_mode='extended', count=1000)
+negative_tweets = tweepy.Cursor(api.search, q=negative_words, tweet_mode='extended', count=1000)
 api.home_timeline()
 count = 0
-for tweet, p_tweet in zip(public_tweets.items(100), positive_tweets.items(100)):
+for tweet, p_tweet in zip(negative_tweets.items(1000), positive_tweets.items(1000)):
     # print(tweet.text)
     if tweet.lang == "en":
-        print(count)
-        
+        # nfull_text = p.clean(tweet.full_text)
+        # pfull_test = p.clean(p_tweet.full_text)
         # Write a row to the CSV file. I use encode UTF-8
-        csvWriter.writerow([tweet.created_at, tweet.full_text.encode('utf-8')])
-        print(tweet.created_at, tweet.full_text)
+        csvWriter.writerow([tweet.created_at, tweet.full_text])
+        csvWriter.writerow([p_tweet.created_at, p_tweet.full_text])
+        # print(tweet.created_at, tweet.full_text)
         count = count + 1
 
 csvFile.close()
