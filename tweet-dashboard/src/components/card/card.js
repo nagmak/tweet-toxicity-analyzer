ec3638b6dd647a8c4f57fea309450313b51f4392
Nagma Kapoor <nagmakapoor@gmail.com>
2019-12-06 17:46:42 -0500

    added pie chart

--- tweet-dashboard/src/components/card/card.js
+++ tweet-dashboard/src/components/card/card.js
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import PieChartSimple from '../pie-chart/pie-chart';
 
 const useStyles = makeStyles({
   card: {
@@ -30,24 +31,7 @@ export default function SimpleCard() {
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <PieChartSimple></PieChartSimple>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
