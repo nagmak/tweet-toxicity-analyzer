2be33e12bebdfffb9a02fecb7d3bf649c11efeb6
Nagma Kapoor <nagmakapoor@gmail.com>
2019-12-06 17:31:51 -0500

    Added app-bar, card and grid

---
+++ tweet-dashboard/src/main/main.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import SimpleCard from '../components/card/card'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+            <SimpleCard></SimpleCard>
+        </Grid>
+        <Grid item xs={6}>
+            <SimpleCard></SimpleCard>
+        </Grid>
+        <Grid item xs={6}>
+            <SimpleCard></SimpleCard>
+        </Grid>
+        <Grid item xs={6}>
+            <SimpleCard></SimpleCard>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
